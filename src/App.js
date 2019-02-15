40c70aa8205a9612822f02fbc502c3f53c23a8f6
Abdelmajid Essofi <abdelmajidesofi01@gmail.com>
2019-02-15 11:13:28 +0100

    First commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import LeftBar from './leftbar';
+import {Form, Button} from 'react-bootstrap';
+
+import './App.css';
+import Login from "./login";
+
+/**
+ * @return {boolean}
+ */
+export default function App() {
+
+  return (
+      <Login/>
+  );
+}
